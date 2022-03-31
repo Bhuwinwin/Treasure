ac5c3e1047d97502bc2c71db50aa9455a7636c4d
Winn <iambhuwin@gmail.com>
2022-03-31 11:07:20 +0700

    Initial Commit

---
+++ Treasure/TreasureApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreasureApp.swift
+//  Treasure
+//
+//  Created by Pumai Vittayanukorn on 31/3/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct TreasureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
